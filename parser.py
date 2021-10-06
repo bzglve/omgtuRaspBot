71785d771c4c2e408f143da87cf4faa7eb51700b
viktory683@gmail.com <viktory683@gmail.com>
2021-10-06 22:11:01 +0600

    done. going to bed now.

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
-from requests import get
 import json
+
+from requests import get
+
 page = get('https://rasp.omgtu.ru/api/schedule/group/464')
 print(page.status_code)
 shedule = json.loads(page.content.decode('utf-8'))
@@ -7,4 +9,7 @@ print(type(shedule))
 for i in shedule:
     for key in i.keys():
         print(key, i[key])
-    print('#'*10)
+    print('#' * 10)
+
+# TODO Найти поддомены shedule/... для получения по преподам.группам и тп
+# TODO По-человечески парсить
