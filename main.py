c9450aa413296eb2c4600adbd7a84ee5475b2cbe
viktory683@gmail.com <viktory683@gmail.com>
2021-10-07 21:58:08 +0600

    REALLY FUCKING FIXED

--- main.py
+++ main.py
@@ -13,26 +13,44 @@ with open(private_variables_file, 'r') as pv:
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-start_commands = [
-    'start',
-    'help'
-]
-
-other_commands = [
+bot_commands_keys = {
     ''
-]
+    'Выбор группы'
+}
 
 
-@dp.message_handler(commands=start_commands)
+@dp.message_handler(commands=['start'])
 async def send_welcome(msg: types.Message):
+    keys = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keys.add(types.KeyboardButton())
     await msg.answer('Приветствую\n\
 Я бот расписания омского политеха\n\
-Пока что довольно тупой, но разрабы постепенно меня прокачают')
+Пока что довольно тупой, но разрабы постепенно меня прокачают', reply_markup=keys)
+
+
+@dp.message_handler(commands=['help'])
+async def send_help(msg: types.Message):
+    await msg.answer('/groups Выведет список всех групп')
+
+
+@dp.message_handler(commands=['group'])
+async def send_groups(msg: types.Message):
+    await msg.answer('Напиши номер своей группы')
+    await msg.answer('Например "БИТ-201...')
 
 
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
-    await msg.answer('Да падажжи падла, я ещё них** не умею')
+    print(msg)
+    print(msg.text.lower())
+    if 'да' == msg.text.lower():
+        await msg.answer('Ну я тебя предупреждал')
+        with open('variables.json', 'r') as f:
+            groups = list(dict(json.load(f)).keys())
+            await msg.answer((' '.join(groups))[:4096])
+            await msg.answer((' '.join(groups))[4096:4096 * 2])
+    else:
+        await msg.answer('Да падажжи падла, я ещё них** не умею')
 
 
 def main():
--- parser.py
+++ parser.py
@@ -2,14 +2,39 @@ import json
 
 from requests import get
 
-page = get('https://rasp.omgtu.ru/api/schedule/group/464')
-print(page.status_code)
-shedule = json.loads(page.content.decode('utf-8'))
-print(type(shedule))
-for i in shedule:
-    for key in i.keys():
-        print(key, i[key])
-    print('#' * 10)
+# page_raw = get('https://rasp.omgtu.ru/api/schedule/group/464')
+page_raw = get('https://rasp.omgtu.ru/api/schedule/group/39')
+print(page_raw.status_code)
+page = json.loads(page_raw.content.decode('utf-8'))
+
+keys = [
+    'auditorium',
+    'beginLesson',
+    'date',
+    'dayOfWeekString',
+    'discipline',
+    'endLesson',
+    'kindOfWork',
+    'lecturer',
+    'stream',
+    'group',
+    'subGroup'
+]
+
+groups_candidates = []
+
+for p in page:
+    # print(p)
+    if p['dayOfWeekString'] in ['Чт', 'Пт'] or True:
+        for key in keys:
+            if p[key] is not None:
+                print(p[key])
+                if key in ['stream', 'group', 'subGroup']:
+                    if p[key] not in groups_candidates:
+                        groups_candidates.append(p[key])
+        print('#' * 10)
+
+print(groups_candidates)
 
 # TODO Найти поддомены shedule/... для получения по преподам.группам и тп
 # TODO По-человечески парсить
